97616dc683cc789bdbd06204c9ef5ee6f13e60f4
Hasansarwer <hasanmathju@gmail.com>
2024-12-23 01:33:19 +0600

    Correction Task 7

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
+
     # TODO
      # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
@@ -25,8 +26,12 @@ def sent_analyzer():
     # Extract the label and score from the response
     label = response['label']
     score = response['score']
-    # Return a formatted string with the sentiment label and score
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+        # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the sentiment label and score
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
